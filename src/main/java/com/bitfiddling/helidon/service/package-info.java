7b83ba911952427a2778601972d6d14eb9a55bfc
Varun Madiath <github@madiathv.com>
2025-07-23 16:24:12 -0400

    Initial commit of Helidon quickstart

---
+++ src/main/java/com/bitfiddling/helidon/service/package-info.java
@@ -0,0 +1,1 @@
+package com.bitfiddling.helidon.service;
---
+++ src/test/java/com/bitfiddling/helidon/service/MainTest.java
@@ -0,0 +1,11 @@
+package com.bitfiddling.helidon.service;
+
+import io.helidon.webserver.testing.junit5.DirectClient;
+import io.helidon.webserver.testing.junit5.RoutingTest;
+
+@RoutingTest
+class MainTest extends AbstractMainTest {
+    MainTest(DirectClient client) {
+        super(client);
+    }
+}
